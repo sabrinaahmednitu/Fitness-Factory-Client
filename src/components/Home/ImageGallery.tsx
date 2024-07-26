59ebf75b555935baefffb7cf3e2ddbbf43009306
sabrina <sabrinaahmednitu@gmail.com>
2024-07-26 23:20:28 +0600

    mosaic done

--- src/components/Home/ImageGallery.tsx
+++ src/components/Home/ImageGallery.tsx
@@ -4,28 +4,21 @@ import p2 from '../../assets/images/HDStockImages.com-mZQ2iq.jpg';
 import p3 from '../../assets/images//expertTraine.jpeg';
 import p5 from '../../assets/images/wideVarity.jpg';
 import p8 from '../../assets/images/heroSection.jpg';
-import p9 from '../../assets/images//history.jpg';
 import p10 from '../../assets/images/mission.jpg';
 import p14 from '../../assets/images/heroSection.jpg';
-import p16 from '../../assets/images/vision.jpg';
+
 import React from 'react';
 import './ImageGallery.css'
 
 const images = [
   { src: p1, alt: 'Image 1', className: '' },
-  { src: img1, alt: 'Image 21', className: 'row-span-2' },
+  { src: img1, alt: 'Image 21', className: '' },
   { src: p14, alt: 'Image 18', className: 'col-span-2' },
-  { src: p10, alt: 'Image 22', className: '' },
-  { src: p5, alt: 'Image 5', className: 'col-span-2' },
-  { src: p8, alt: 'Image 8', className: 'col-span-2 row-span-2' },
+  { src: p10, alt: 'Image 22', className: 'col-span-2' },
+  { src: p5, alt: 'Image 5' },
+  { src: p8, alt: 'Image 8', className: 'row-span-2' },
   { src: p3, alt: 'Image 3', className: '' },
-  { src: p2, alt: 'Image 2', className: 'row-span-2' },
-  { src: p9, alt: 'Image 6', className: '' },
-  { src: p16, alt: 'Image 16', className: 'col-span-2' },
-  { src: p3, alt: 'Image 3', className: '' },
-  { src: p2, alt: 'Image 2', className: 'row-span-2' },
-  { src: p9, alt: 'Image 6', className: '' },
-  { src: p16, alt: 'Image 16', className: 'col-span-2' },
+  { src: p2, alt: 'Image 2', className: 'col-span-2' },
 ];
 
 const ImageGallery = () => {
@@ -62,14 +55,14 @@ const ImageGallery = () => {
         </div>
       )}
 
-      <div className="container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-1 auto-rows-min">
+      <div className="container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 auto-rows-min">
         {images.map((image, index) => (
           <div key={index} className={image.className}>
             <img
               src={image.src}
               alt={image.alt}
               onClick={() => openFullimg(image.src)}
-              className="w-full h-full object-cover cursor-pointer"
+              className="w-full h-full object-cover cursor-pointer rounded-lg"
             />
           </div>
         ))}
