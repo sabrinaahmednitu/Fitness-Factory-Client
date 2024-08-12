9b152d568efe597a6107e655374a210fbfbfbb5b
sabrina <sabrinaahmednitu@gmail.com>
2024-08-12 15:43:45 +0600

    success mark

--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
+import 'animate.css';
+
 const Success = () => {
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center min-h-screen bg-green-50">
       <div className="text-center p-10 bg-white shadow-lg rounded-lg animate__animated animate__bounceIn">
-        <div className="bg-black rounded-2xl">
+        <div className="rounded-full h-[120px] w-[120px] border-4 border-green-500 mx-auto ">
           <svg
-            className="w-16 h-16 text-green-500 mx-auto mb-4 animate__animated animate__tada animate__delay-1s"
+            className="w-18 h-24 text-green-500 mt-2 mx-auto animate__animated animate__tada animate__delay-1s"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -20,19 +22,19 @@ const Success = () => {
             />
           </svg>
         </div>
-        <h2 className="text-3xl font-bold text-gray-800 mb-2 animate__animated animate__fadeIn animate__delay-2s">
+        <h2 className="text-3xl font-bold text-gray-800 mb-2 mt-3 ">
           Congratulations !
         </h2>
-        <h2 className="text-3xl font-bold text-green-500 mb-2 animate__animated animate__fadeIn animate__delay-2s">
+        <h2 className="text-3xl font-bold text-gray-800 mb-2">
           Order Placed Successfully!
         </h2>
 
-        <p className="text-gray-600 animate__animated animate__fadeInUp animate__delay-3s">
+        <p className="text-gray-600">
           Thank you for your purchase. We are processing your order.
         </p>
         <button
           onClick={() => navigate('/')}
-          className="mt-6 px-5 py-3 bg-gray-500 text-white font-bold rounded-lg hover:bg-green-600 transition duration-300 ease-in-out animate__animated animate__fadeInUp animate__delay-4s"
+          className="mt-6 px-5 py-3 bg-gray-500 text-white font-bold rounded-lg hover:bg-green-600 "
         >
           Back to Home
         </button>
